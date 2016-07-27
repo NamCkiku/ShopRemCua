8e8ded0c24e84307b62e60fb8b7e4fcea7f30b3e
NamCkiku <tranhoangnam11373@gmail.com>
2016-07-27 15:56:27 +0700

    add database

---
+++ RemCua.Entities/Models/ShopRemCuaEntity.cs
@@ -0,0 +1,81 @@
+namespace RemCua.Entities.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ShopRemCuaEntity : DbContext
+    {
+        public ShopRemCuaEntity()
+            : base("name=ShopRemCuaEntity")
+        {
+        }
+
+        public virtual DbSet<Error> Errors { get; set; }
+        public virtual DbSet<Feedback> Feedbacks { get; set; }
+        public virtual DbSet<Footer> Footers { get; set; }
+        public virtual DbSet<MenuGroup> MenuGroups { get; set; }
+        public virtual DbSet<Menu> Menus { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Page> Pages { get; set; }
+        public virtual DbSet<PostCategory> PostCategories { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<ProductCategory> ProductCategories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Slide> Slides { get; set; }
+        public virtual DbSet<SupportOnline> SupportOnlines { get; set; }
+        public virtual DbSet<SystemConfig> SystemConfigs { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+        public virtual DbSet<VisitorStatistic> VisitorStatistics { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MenuGroup>()
+                .HasMany(e => e.Menus)
+                .WithRequired(e => e.MenuGroup)
+                .HasForeignKey(e => e.GroupID);
+
+            modelBuilder.Entity<Page>()
+                .Property(e => e.Alias)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<PostCategory>()
+                .Property(e => e.Alias)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<PostCategory>()
+                .HasMany(e => e.Posts)
+                .WithRequired(e => e.PostCategory)
+                .HasForeignKey(e => e.CategoryID);
+
+            modelBuilder.Entity<Post>()
+                .Property(e => e.Alias)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Post>()
+                .HasMany(e => e.Tags)
+                .WithMany(e => e.Posts)
+                .Map(m => m.ToTable("PostTags").MapLeftKey("PostID").MapRightKey("TagID"));
+
+            modelBuilder.Entity<ProductCategory>()
+                .HasMany(e => e.Products)
+                .WithRequired(e => e.ProductCategory)
+                .HasForeignKey(e => e.CategoryID);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.Tags1)
+                .WithMany(e => e.Products)
+                .Map(m => m.ToTable("ProductTags").MapLeftKey("ProductID").MapRightKey("TagID"));
+
+            modelBuilder.Entity<SystemConfig>()
+                .Property(e => e.Code)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Tag>()
+                .Property(e => e.ID)
+                .IsUnicode(false);
+        }
+    }
+}
