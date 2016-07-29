1fa64dda4cc9cf14eb108841e0743b1a31e23f70
NamCkiku <tranhoangnam11373@gmail.com>
2016-07-29 09:37:05 +0700

    update index

---
+++ RemCua.Service/PostService.cs
@@ -0,0 +1,73 @@
+﻿using RemCua.Entities.Models;
+using RemCua.Repository.Infrastructure;
+using RemCua.Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemCua.Service
+{
+    public interface IPostService
+    {
+        Post Add(Post post);
+
+        void Update(Post post);
+
+        Post Delete(int id);
+
+        IEnumerable<Post> GetAll();
+
+        IEnumerable<Post> GetNewPost(int top);
+
+        Post GetById(int id);
+
+        void SaveChanges();
+    }
+    public class PostService : IPostService
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PostService(IPostRepository postRepository, IUnitOfWork unitOfWork)
+        {
+            this._postRepository = postRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        public Post Add(Post post)
+        {
+            return _postRepository.Add(post);
+        }
+
+        public Post Delete(int id)
+        {
+            return _postRepository.Delete(id);
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            return _postRepository.GetAll();
+        }
+
+        public Post GetById(int id)
+        {
+            return _postRepository.GetSingleById(id);
+        }
+
+        public IEnumerable<Post> GetNewPost(int top)
+        {
+            return _postRepository.GetMulti(x => x.Status).OrderByDescending(x => x.CreatedDate).Take(top);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(Post post)
+        {
+            _postRepository.Update(post);
+        }
+    }
+}
--- RemCua.Web/Controllers/HomeController.cs
+++ RemCua.Web/Controllers/HomeController.cs
@@ -10,28 +10,20 @@ namespace RemCua.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IProductService _productService;
-        public HomeController(IProductService productService)
+        private readonly IProductCategoryService _productCategoryService;
+        private readonly IPostService _postService;
+        public HomeController(IProductService productService, IProductCategoryService productCategoryService, IPostService postService)
         {
             this._productService = productService;
+            this._productCategoryService = productCategoryService;
+            this._postService = postService;
         }
         public ActionResult Index()
         {
             ViewBag.NewProduct = _productService.ListNewProduct(4);//Hiển Thị theo ViewBag
             ViewBag.Feature = _productService.ListFeatureProduct(6);
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Category = _productCategoryService.GetProductByCategory(5);
+            ViewBag.NewPost = _postService.GetNewPost(6);
             return View();
         }
     }
